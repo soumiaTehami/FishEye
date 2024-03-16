2d0f9a4b776c55c8680a3bf459aaee2e7a73ff9e
tehami soumia <sou_mia_46@hotmail.fr>
2024-03-16 13:58:28 +0100

    modifie function photographer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `Tarif : ${price} € / jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, tagline, price, getUserCardDOM };
 }
