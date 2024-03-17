73da2cb1fc1da25b1e29cefad7ab0d14470d983a
tehami soumia <sou_mia_46@hotmail.fr>
2024-03-17 07:05:49 +0100

    Media queries

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait, tagline, price } = data;
+    const { name, portrait, tagline, price, country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,23 +7,32 @@ function photographerTemplate(data) {
         const article = document.createElement('article');
         const img = document.createElement('img');
         img.setAttribute("src", picture);
+        img.classList.add("photographer-image");
         
         const h2 = document.createElement('h2');
         h2.textContent = name;
-
-        const pTagline = document.createElement('p');
-        pTagline.textContent = tagline;
+        h2.classList.add("photographer-name");
+        
+        const pCountry = document.createElement('p');
+        pCountry.textContent = `Country: ${country}`;
+        pCountry.classList.add("photographer-country");
+        
+        const h3 = document.createElement('h3');
+        h3.textContent = tagline;
+        h3.classList.add("photographer-tagline");
 
         const pPrice = document.createElement('p');
-        pPrice.textContent = `Tarif : ${price} € / jour`;
+        pPrice.textContent = `Price: ${price} € / day`;
+        pPrice.classList.add("photographer-price");
 
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(pTagline);
+        article.appendChild(pCountry);
+        article.appendChild(h3);
         article.appendChild(pPrice);
 
         return article;
     }
 
-    return { name, picture, tagline, price, getUserCardDOM };
+    return { name, picture, tagline, price, country, getUserCardDOM };
 }
