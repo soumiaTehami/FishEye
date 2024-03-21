f5c89dcce96c5c5124152976e6b5fd9194e7e453
tehami soumia <sou_mia_46@hotmail.fr>
2024-03-21 10:01:06 +0100

    fetch photographers

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,25 +1,42 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Mettre le code JavaScript lié à la page photographer.html
 const searchParams = new URLSearchParams(window.location.search);
 
 // Accéder à un paramètre spécifique
 const id = searchParams.get("id");
 
-
 // Utiliser les valeurs des paramètres
 console.log("ID:", id);
-// Récupérer tous les éléments avec la classe "photographer"
-const photographers = document.querySelectorAll(".photographer-card");
 
-// Ajouter un gestionnaire d'événements à chaque photographe
-photographers.forEach((photographer) => {
-  photographer.addEventListener("click", () => {
-    // Récupérer l'ID du photographe à partir de l'attribut data-id
-    const photographerId = photographer.getAttribute("id");
-    // Rediriger vers la page de détails avec l'ID du photographe comme paramètre d'URL
-    window.location.href = `photographer.html?id=${photographerId}`;
-    console.log(photographerId)
-  });
-});
+async function getPhotographerData(id) {
+    try {
+        const response = await fetch(`data/photographers.json`);
+        const data = await response.json();
+        console.log("Données récupérées :", data);
+        const photographer = data.photographers.find(photographer => photographer.id == id);
+        console.log("Données récupérées :", photographer);
+        if (photographer) {
+            return photographer;
+        } else {
+            throw new Error('Photographe non trouvé');
+        }
+    } catch (error) {
+        console.error('Une erreur s\'est produite lors de la récupération des données du photographe :', error);
+    }
+    
+}
 
+// Appel de la fonction pour récupérer les données du photographe
+getPhotographerData(id)
+    .then(data => {
+        if (data) {
+            displayPhotos(data);
+        }
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données du photographe :', error);
+    });
 
+    
 
+    
+    
