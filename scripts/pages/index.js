c813cc5e06797809e7559c8fb20ac68b7a76a9aa
tehami soumia <sou_mia_46@hotmail.fr>
2024-04-12 08:41:55 +0200

    le tri date

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,30 @@
 async function getPhotographers() {
-    // Code pour récupérer les données des photographes
-    const response = await fetch('/data/photographers.json');
-    const data = await response.json();
-    return data.photographers;
+  // Code pour récupérer les données des photographes
+  const response = await fetch("/data/photographers.json");
+  const data = await response.json();
+  return data.photographers;
 }
 
 async function displayData(photographers) {
-    // Vérifier si photographers est défini
-    if (photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+  // Vérifier si photographers est défini
+  if (photographers) {
+    const photographersSection = document.querySelector(
+      ".photographer_section"
+    );
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    photographers.forEach((photographer) => {
+      const photographerModel = photographerTemplate(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM);
+    });
+  }
 }
 
 async function init() {
-    // Récupère les datas des photographes
-    const photographers = await getPhotographers();
-    displayData(photographers);
-    console.log(photographers)
+  // Récupère les datas des photographes
+  const photographers = await getPhotographers();
+  displayData(photographers);
+  console.log(photographers);
 }
 
 init();
-
