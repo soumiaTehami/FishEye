88378b85ea96e4741a36e6f0014a723b3cbdf66d
tehami soumia <sou_mia_46@hotmail.fr>
2024-04-20 09:09:21 +0200

    correction de code

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,10 +1,19 @@
 import { photographerTemplate } from "../templates/photographer.js";
 async function getPhotographers() {
   // Code pour récupérer les données des photographes
-  const response = await fetch("/data/photographers.json");
-  const data = await response.json();
-  return data.photographers;
+    try {
+        const response = await fetch("data/photographers.json");
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des données JSON');
+        }
+        const data = await response.json();
+        return data.photographers;
+    } catch (error) {
+        console.error('Erreur fetch :', error);
+        return []; // Retourner une liste vide en cas d'erreur
+    }
 }
+
 
 async function displayData(photographers) {
   // Vérifier si photographers est défini
