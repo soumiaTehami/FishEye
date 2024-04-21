74ed6f1efd4d820d9c29fb48fd5405b268034b6a
tehami soumia <sou_mia_46@hotmail.fr>
2024-04-21 10:02:19 +0200

    correction de code

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -47,6 +47,25 @@ async function addHeaderToModal() {
 
 addHeaderToModal();
 
+// Assurez-vous que l'URL du fichier JavaScript est correcte
+const scriptUrl = "scripts/pages/photographer.js";
+
+// Chargez le script JavaScript
+fetch(scriptUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Erreur de chargement du script : " + response.status);
+    }
+    return response.text();
+  })
+  .then(scriptText => {
+    // Exécutez le script une fois qu'il est chargé
+    eval(scriptText);
+  })
+  .catch(error => {
+    console.error("Une erreur s'est produite lors du chargement du script :", error);
+  });
+
 getPhotographerData(id)
   .then((photographerData) => {
     if (photographerData) {
